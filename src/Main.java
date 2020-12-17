dab0a8225c39cf528071a641b94b8a2b180a25d2
Aliaksandr Tarauski <a.taravsky@gmail.com>
2020-12-17 16:10:05 +0300

    5.0

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Пока");
         System.out.println("Мир");
-        System.out.println("!");
+        System.out.println("Ошибка");
     }
 }
