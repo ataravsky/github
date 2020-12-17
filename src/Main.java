aee2f9a5d3f60f41e33581e00b8e9d0319c3a8ed
Aliaksandr Tarauski <a.taravsky@gmail.com>
2020-12-17 16:01:44 +0300

    new branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет");
+        System.out.println("Пока");
         System.out.println("Мир");
         System.out.println("!");
     }
